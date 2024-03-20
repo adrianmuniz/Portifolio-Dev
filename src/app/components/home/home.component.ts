824f12e46095113e2724b8849604bd4f660373e0
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2024-03-20 00:37:14 -0300

    Introdução

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage, NavBarComponent ],
+  imports: [NgOptimizedImage, NavBarComponent, ContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
