72305c31247d7f4f08e1ff8527a9af29ab7c5b84
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2024-03-26 17:13:38 -0300

    Subindo código Inicial

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/meu-portifolio'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/meuPortifolio'));
 
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {
+    root: 'dist/meuPortifolio/'
+  }),
+);
 
-  res.sendFile(path.join(__dirname + '/dist/meu-portifolio/browser/index.html'));
-});
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
