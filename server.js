2259a71e16009faa5f6b242c80bbb724b8b3160b
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2024-03-26 17:22:30 -0300

    Teste Deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/meuPortifolio'));
+app.use(express.static('./dist/meu-portifolio'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {
-    root: 'dist/meuPortifolio/'
+    root: 'dist/meu-portifolio/'
   }),
 );
 
