31254ab96a38b92a2922c4e675284e12bed1e5ce
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2024-03-26 17:33:04 -0300

    Test Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/meu-portifolio'));
+app.use(express.static('./dist/meuPortifolio'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {
-    root: 'dist/meu-portifolio/'
+    root: 'dist/meuPortifolio/'
   }),
 );
 
